af33c522debc479a7482256ba2f17ba599667243
Stefan Dunn <stefannicholadsdunn@googlemail.com>
2023-07-30 15:38:28 +0100

    - Improve image rendering - Device detection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,27 @@
 import Image from "next/image";
 import pizzaImage from "../../public/images/pizza-weather.jpg";
-import { LocatorButton } from "@/components/LocatorButton/LocatorButton";
 import { LocationForm } from "@/components/LocationForm/LocationForm";
+import { headers } from "next/headers";
+import { getDevice } from "@/utils/helpers";
 
 export default function Home() {
+  const isMobile =
+    getDevice(headers().get("user-agent") as string) !== "desktop";
+
   return (
     <main className="flex justify-stretch md:items-stretch md:justify-normal flex-col-reverse md:flex-row flex-grow">
       <div className="md:w-1/2 md:h-auto basis-1/2 flex-grow relative">
         <Image
           src={pizzaImage}
           alt="Pizza graphic"
-          width={pizzaImage.width / 4}
-          height={pizzaImage.height / 4}
+          width={pizzaImage.width / (isMobile ? 6 : 4)}
+          height={pizzaImage.height / (isMobile ? 6 : 4)}
           className="object-cover object-center absolute left-0 top-0 w-full h-full"
           priority
         />
       </div>
-      <div className="md:w-1/2 md:h-auto basis-1/2 flex-grow relative flex items-center justify-center">
-        <div className="text-center px-5">
+      <div className="p-3 md:w-1/2 md:h-auto basis-1/2 flex-grow relative flex items-center justify-center">
+        <div className="text-center">
           <h1 className="text-pink">Pizza Forecast</h1>
           <h2 className="text-indigo">
             Find the best time to{" "}
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,7 +1,15 @@
+import UAParser from "ua-parser-js";
+
 export const getOrElse = <T = any>(valFunction: () => T): T | undefined => {
   try {
     return valFunction();
   } catch {
     return undefined;
   }
+};
+
+export const getDevice = (userAgent?: string) => {
+  const parser = new UAParser(userAgent);
+  const { type } = parser.getDevice();
+  return type || "desktop";
 };
